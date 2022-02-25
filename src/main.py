e5df560de87f822bb1bd8eb95c128949bb9d6f76
John Cho <johnnychox@gmail.com>
2022-02-24 23:53:20 -0500

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,69 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matrixtools import Matrix
+import csv
+
+with open('input\\inputData.csv') as f:
+    inputs = [list(map(float, i)) for i in list(csv.reader(f))[1:]]
+    n = len(inputs)
+    f.close()
+
+
+def f(x, order, coeffs):
+    y = 0
+    for coeff in coeffs:
+        y += coeff * (x ** order)
+        order -= 1
+    return y
+
+
+def main():
+    # WARNING... insufficient number of points may not produce a graph
+    degree = 2
+    x_max = float('-inf')
+    x_min = float('inf')
+    y_vector = Matrix([], n, 1)
+    X_matrix = Matrix([], n, degree + 1)
+    for x, y in inputs:
+        if x > x_max:
+            x_max = x
+        if x < x_min:
+            x_min = x
+        y_vector.add_row([y])
+        X_matrix.add_row([1.0] + [x ** i for i in range(1, degree + 1)])
+    
+    # p_vector contains the approximate coefficients of the polynomial 
+    # that best models a set of points
+    p_vector = ((X_matrix.transpose() * X_matrix).inverse() * X_matrix.transpose()) * y_vector
+    coe = sum(p_vector.matrix, [])[::-1]
+
+    # Print out results
+    print('Polynomial model order =', degree, end='\n\n')
+    print('f(x) = ' + ' + '.join(f'{chr(97 + i)}*x^{degree - i}' for i in range(degree + 1)), end='\n\n')
+    for i in range(degree + 1):
+        print(f'{chr(97 + i)} = {coe[i]}')
+
+    # GRAPHING
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.spines['left'].set_position('center')
+    ax.spines['bottom'].set_position('center')
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+
+    x = np.linspace(x_min, x_max, 100)
+
+    # Plot line
+    plt.plot(x, f(x, degree, coe))
+
+    # Plot points
+    plt.scatter(*list(zip(*inputs)))
+
+    # Show plot
+    plt.show()
+    
+
+if __name__ == '__main__':
+    main()
---
+++ src/matrixtools.py
@@ -0,0 +1,84 @@
+from __future__ import annotations
+from typing import List
+
+
+class Matrix:
+    def __init__(self, matrix: List[List[float]], rows: int, cols: int):
+        self.matrix = [self.__zfill_list(i, cols) for i in matrix[:rows]]
+        self.rows = rows
+        self.cols = cols
+        self.null_rows = rows - len(matrix)
+
+        for _ in range(self.null_rows):
+            self.matrix.append([0] * self.cols)
+
+
+    def add_row(self, row: List[float]) -> None:
+        if self.null_rows > 0:
+            # All rows should be of the same size
+            row = self.__zfill_list(row, self.cols)
+            self.matrix[-self.null_rows] = row
+            self.null_rows -= 1
+    
+    def __str__(self):
+        return f'{self.matrix}'
+
+    # Matrix Multiplication
+    def __mul__(self, other: Matrix) -> Matrix:
+        C = []
+        n = self.rows
+        m = self.cols
+        p = other.cols
+        
+        # Return empty matrix if number of columns
+        # From matrix A != number of rows from matrix B
+        if m != other.rows:
+            return Matrix([], 0, 0)
+
+        for i in range(n):
+            C.append([])
+            for j in range(p):
+                sum = 0
+                for k in range(m):
+                    sum += self.matrix[i][k] * other.matrix[k][j]
+                C[i].append(sum)
+        
+        return Matrix(C, self.rows, other.cols)
+
+    
+    def transpose(self) -> Matrix:
+        return Matrix([*map(list, zip(*self.matrix))], self.cols, self.rows)
+    
+    def inverse(self) -> Matrix:
+        # Must be a square matrix
+        if self.rows != self.cols:
+            return Matrix([], 0, 0)
+        
+        # Find inverse using Gauss-Jordan method
+        n = self.rows
+        A = [i + [0] * n for i in self.matrix]
+        inverse = []
+        for i in range(n):
+            for j in range(n):
+                if i == j:
+                    A[i][j + n] = 1
+
+        for i in range(n):
+            for j in range(n):
+                if i != j:
+                    ratio = A[j][i] / A[i][i]
+                    for k in range(2 * n):
+                        A[j][k] -= ratio * A[i][k]
+        
+        for i in range(n):
+            inverse.append([])
+            for j in range(n, 2 * n):
+                A[i][j] /= A[i][i]
+                inverse[i].append(A[i][j])
+        
+        return Matrix(inverse, n, n)
+
+
+    def __zfill_list(self, l: List[float], n: int) -> List[float]:
+        return l[:n] + [0] * (n - len(l))
+    
